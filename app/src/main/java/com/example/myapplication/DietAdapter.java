192d21b7824d60b178ee8def82718f83285b68e0
sandeep <sandeepprabhakula10@gmail.com>
2021-05-10 10:23:10 +0530

    web view feature added

--- app/src/main/java/com/example/myapplication/DietAdapter.java
+++ app/src/main/java/com/example/myapplication/DietAdapter.java
@@ -9,6 +9,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -34,7 +35,10 @@ public class DietAdapter extends RecyclerView.Adapter<DietAdapter.ViewHolder> {
         holder.textView11.setText(diet.getDiet());
         holder.linearLayout.setOnClickListener(v -> {
             String s = "https://www.healthline.com/health/"+holder.textView11.getText().toString().toLowerCase()+"?ref=global";
-            v.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(s)));
+//            v.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(s)));
+            CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+            CustomTabsIntent customTabsIntent = builder.build();
+            customTabsIntent.launchUrl(v.getContext(),Uri.parse(s));
         });
     }
 
@@ -43,13 +47,20 @@ public class DietAdapter extends RecyclerView.Adapter<DietAdapter.ViewHolder> {
         return diets.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder implements  View.OnClickListener{
         private final TextView textView11;
         private final LinearLayout linearLayout;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             textView11 = itemView.findViewById(R.id.textView11);
             linearLayout = itemView.findViewById(R.id.linearLayout);
+            linearLayout.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            String s = "https://www.healthline.com/health/"+textView11.getText().toString().toLowerCase()+"?ref=global";
+            v.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(s)));
         }
     }
 }
